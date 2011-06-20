20842c3dc4307d41b3008120b04a1a9a47a0f631
Kai Rubarth <krubar@gmail.com>
2011-06-20 14:48:31 -0400

    forgot to require test adapter

--- lib/sms_gateway.rb
+++ lib/sms_gateway.rb
@@ -4,6 +4,7 @@ require 'rails/generators'
 require 'sms_gateway/base'
 require 'sms_gateway/sms'
 require 'sms_gateway/adapters/sms_global'
+require 'sms_gateway/adapters/test'
 require 'sms_gateway/jobs/sms_job'
 require 'sms_gateway/generators/install/install_generator'
 
